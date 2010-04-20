bcfea7294e545f5adf41e26576884e6fab8ba68a
sclark39 <sclark@freolic.com>
2010-04-20 12:05:45 -0700

    Simple Manage DB UI Built

--- GeekDroid/src/cs585_hw3/team33/manage/ManageActivity.java
+++ GeekDroid/src/cs585_hw3/team33/manage/ManageActivity.java
@@ -1,7 +1,12 @@
 package cs585_hw3.team33.manage;
 
+import cs585_hw3.team33.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ManageActivity extends Activity {
-
+	public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage);
+	}
 }
