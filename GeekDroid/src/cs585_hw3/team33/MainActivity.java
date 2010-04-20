d89ed235ac360ba9065a4418d6b27ae5a503e430
sclark39 <sclark@freolic.com>
2010-04-20 11:08:58 -0700

    GeekDroid interface initial project creation (hello world)

---
+++ GeekDroid/src/cs585_hw3/team33/MainActivity.java
@@ -0,0 +1,13 @@
+package cs585_hw3.team33;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
